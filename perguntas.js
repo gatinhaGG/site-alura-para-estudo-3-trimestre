58881295abcf6a84f1c260dc2616b2c7e9c8f124
gatinhaGG <141866444+gatinhaGG@users.noreply.github.com>
2024-11-27 22:05:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital da Irlanda é Tokyo'
+)
+
+criaCartao(
+    'Programação',
+    'O que são estruturas de decisão compostas?',
+    'Como o if-else e elif. Essas estruturas permitem tomar decisões no seu código, executando diferentes blocos de código dependendo de uma condição.'
+)
+
+criaCartao(
+    'Linguagem japonêsa',
+    'Como se diz Boa Tarde em Japonês ?',
+    'Em Japonês, "Boa tarde" se diz こんにちは (pronuncia-se "Konnichiwa").'
+)
